e753c596e2715020329f98402ad6e9d3bef394fc
EvelinaVirda <for.alvi.development@gmail.com>
2020-06-25 11:29:57 +0700

    model baru view

---
+++ app/Controllers/Pages.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Controllers;
+
+class Pages extends BaseController
+{
+    public function index()
+    {
+        $data = [
+            'title'   => 'home | WebProgrammingUNPAS',
+            'tes' => ['satu', 'dua', 'tiga']
+        ];
+        return view('Pages/home', $data);
+    }
+
+    public function about()
+    {
+        $data = [
+            'title'   => 'About Me'
+        ];
+        return view('Pages/about', $data);
+    }
+
+    public function contact()
+    {
+        $data = [
+            'title'   => 'Contact Us',
+            'alamat' => [
+                [
+                    'tipe' => 'rumah',
+                    'alamat' => 'graha',
+                    'kota' => 'bandung'
+                ],
+                [
+                    'tipe' => 'kantor',
+                    'alamat' => 'hagrag',
+                    'kota' => 'tambuncity'
+                ]
+            ]
+        ];
+
+        return view('Pages/contact', $data);
+    }
+
+    //--------------------------------------------------------------------
+
+}
